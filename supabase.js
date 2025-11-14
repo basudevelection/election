d594d31d4ea5de171478e58153e3c2e8c4f95dc3
basudevelection <basudevbhandari336@gmail.com>
2025-11-14 22:24:43 +0545

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,3 @@
-
-const SUPABASE_URL = 'https://nslyproryuoffhuihzdu.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5zbHlwcm9yeXVvZmZodWloemR1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMxMTc2MjksImV4cCI6MjA3ODY5MzYyOX0.keX3eiGVY_NmFSkGEbHAL3QSChFhj4jRPWPh56Ai1q0';
-
-const { createClient } = window.supabase;
-window.supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const SUPABASE_URL = 'https://uujelykppkcpgcedhmox.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV1amVseWtwcGtjcGdjZWRobW94Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMxMzIzMzMsImV4cCI6MjA3ODcwODMzM30.-vQgtTZ4nY3SHDE-jJ09p-notl0BfP-UVzmf3AI3Hhk';
+const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
