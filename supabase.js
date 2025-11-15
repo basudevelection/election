1e4915446ce0f4c52320dea44f161d9f5cdf26e2
basudevelection <basudevbhandari336@gmail.com>
2025-11-15 19:59:31 +0545

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,3 +1,7 @@
-const SUPABASE_URL = 'https://uujelykppkcpgcedhmox.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV1amVseWtwcGtjcGdjZWRobW94Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMxMzIzMzMsImV4cCI6MjA3ODcwODMzM30.-vQgtTZ4nY3SHDE-jJ09p-notl0BfP-UVzmf3AI3Hhk';
-const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://uujelykppkcpgcedhmox.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV1amVseWtwcGtjcGdjZWRobW94Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMxMzIzMzMsImV4cCI6MjA3ODcwODMzM30.-vQgtTZ4nY3SHDE-jJ09p-notl0BfP-UVzmf3AI3Hhk'; // Replace with the actual anon key from Supabase API settings
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export { supabase };
