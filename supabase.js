aced24b6481e20e5f2089ad22c870222f74ce82c
basudevelection <basudevbhandari336@gmail.com>
2025-11-15 20:04:26 +0545

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
+// Replace with your actual Supabase project URL and anon key
 const supabaseUrl = 'https://uujelykppkcpgcedhmox.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV1amVseWtwcGtjcGdjZWRobW94Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMxMzIzMzMsImV4cCI6MjA3ODcwODMzM30.-vQgtTZ4nY3SHDE-jJ09p-notl0BfP-UVzmf3AI3Hhk'; // Replace with the actual anon key from Supabase API settings
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export { supabase };
